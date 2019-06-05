299ddbdcdb4d59be2fbcf5d696c5c9250fba11d6
Kevin JY Cui <info@kevincui.dev>
2019-06-05 14:54:57 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-import serial
+mport serial
 import json
 from websocket import create_connection
 import ssl
@@ -12,28 +12,7 @@ with contextlib.redirect_stdout(None):
 
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 receivedData = create_connection("wss://emotivcortex.com:54321", sslopt={"cert_reqs": ssl.CERT_NONE})
-##static_display = ""
-
-##while True:
-##    try:
-##        port = input("Input port name\n--> COM")
-##        static_display += "Input port name\n--> COM"+port+"\n"
-##        if not port.isdigit():
-##            print("Invalid Serial")
-##            static_display += "Invalid Serial\n"
-##            continue
-##        port = "COM"+port
-##        ser = serial.Serial(port, 9600)
-##        print("Successfully Connected!")
-##        static_display += "Successfully Connected!\n"
-##        break
-##    except serial.serialutil.SerialException:
-##        print(port+" Not Found")
-##        static_display += port+" Not Found\n"
-##pygame.init()
-##pygame.display.set_caption("Mr. Face")
-##window = pygame.display.set_mode((220, 180))
-##img = pygame.transform.scale(pygame.image.load("imag.jpg"), (220, 180)).convert_alpha()
+ser = serial.Serial('COM7', 9600)
 
 def setup():
     receivedData.send(json.dumps({
@@ -92,51 +71,22 @@ def get_value():
 setup()
 
 exit_flag = False
-##window.fill((0, 0, 0))
-##window.blit(img, (0, 0))
 
 val_dict = {
     'neutral': 0,
-    'surprise': 1,
+    'blink': 1,
+    'winkL': 2,
+    'winkR': 3,
+    'suprise': 1,
     'frown': 2,
     'smile': 1,
-    'laugh': 2,
-    'blink': 3,
-    'winkR': 2,
-    'winkL': 1,
-    'lookR': 4,
-    'lookL': 5
-    }
-def_dict = {
-    'surprise': 'raised',
-    'frown': 'furrowed',
-    'winkR': 'right wink',
-    'winkL': 'left wink',
-    'lookR': 'right look',
-    'lookL': 'left look'
-    }
-
-index = 0;
+    'laugh': 1,
+   } 
 
 while not exit_flag and get_value()!=-1:
-##    pygame.display.update()
     data = get_value()
-    data1 = data[1]
-    data0 = data[0]
-    if data[1] in def_dict:
-        data1 = def_dict[data[1]]
-    if data[0] in def_dict:
-        data0 = def_dict[data[0]]
-    face = upper_img[val_dict[data[1]]]+" "+"\t\t"+data1+"\n"+eye_img[val_dict[data[0]]]+" \t\t"+data0+"\n"+lower_img[val_dict[data[3]]]+" \t\t\t"+data[3]
-    print("\n"*15+face+"\n"*15)
-
-##    for d in data:
-##        if str(d).isalpha():
-##            ser.write(str(val_dict[d]).encode())
-##        else:
-##            ser.write(bytes(round(d*10)))
-##    for event in pygame.event.get():
-##        if event.type == pygame.QUIT:
-##            exit_flag = True
-
-##pygame.quit()
+    for d in data:
+        if d in val_dict:
+            ser.write(val_dict[d].encode())
+        else:
+            ser.write(0.encode())
